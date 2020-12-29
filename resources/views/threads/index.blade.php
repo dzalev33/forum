a2842fa0854abc11b91d30c7bbc96e330fc2a427
Dzalev <dzalev@hotmail.com>
2020-12-29 12:48:05 +0100

    a thread can have replies

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Forunm THreads</div>
+                    <div class="panel-heading">Forum Threads</div>
 
                     <div class="panel-body">
                      @foreach($threads as $thread)
