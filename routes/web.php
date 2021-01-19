da759982523c60c6c3b551094f365ae77ed973dd
Dzalev <dzalev@hotmail.com>
2021-01-19 14:48:21 +0100

    a user can publish threads

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/home', 'HomeController@index');
 Route::get('/threads', 'ThreadsController@index');
+Route::post('/threads', 'ThreadsController@store');
 Route::get('/threads/{thread}', 'ThreadsController@show');
-
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
