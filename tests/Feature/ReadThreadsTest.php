277362fc701bd96e167b8e52ad4dd83c5e2ec536
Dzalev <dzalev@hotmail.com>
2021-02-06 17:18:37 +0100

    a user can filter threads

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -12,21 +12,23 @@ class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function setup(){
+    public function setup()
+    {
         parent::setup();
-       $this->thread = factory('App\Thread')->create();
+        $this->thread = factory('App\Thread')->create();
     }
 
     /** @test */
     public function test_a_user_can_see_all_threads()
     {
 
-         $this->get('/threads')
-             ->assertSee($this->thread->title);
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
     }
 
     /** @test */
-    public function test_a_user_can_see_single_thread(){
+    public function test_a_user_can_see_single_thread()
+    {
 
         $this->get($this->thread->path())
             ->assertSee($this->thread->title);
@@ -35,11 +37,11 @@ class ReadThreadsTest extends TestCase
     /** @test */
     public function test_a_user_can_read_replies_that_are_associated_with_a_thread()
     {
-      $reply = factory('App\Reply')
-          ->create(['thread_id' => $this->thread->id]);
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
 
-      $this->get($this->thread->path())
-          ->assertSee($reply->body);
+        $this->get($this->thread->path())
+            ->assertSee($reply->body);
 
 
     }
@@ -56,5 +58,18 @@ class ReadThreadsTest extends TestCase
         $this->get('threads/' . $channel->slug)
             ->assertSee($threadInChannel->title)
             ->assertDontSee($threadNotInChannel->title);
+    }
+
+    /** @test */
+    function a_user_can_filter_threads_by_any_username()
+    {
+        $this->signIn(create('App\User', ['name' =>'JohnDoe']));
+
+        $threadByJohn = create('App\Thread', ['user_id' => auth()->id()]);
+        $threadsNotByJohn = create('App\Thread');
+
+        $this->get('threads?by=JohnDoe')
+            ->assertSee($threadByJohn->title)
+            ->assertDontSee($threadsNotByJohn->title);
     }
 }
