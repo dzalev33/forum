ff8dc0147897a9b59b4720952e8adacde63ccc7c
Dzalev <dzalev@hotmail.com>
2021-02-06 14:47:29 +0100

    channel service provider

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
                         <li class="dropdown nav-item">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-has-popup="true" aria-expanded="false">Channels <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                @foreach(App\Channel::all() as $channel)
+                                @foreach($channels as $channel)
                                     <li><a href="/threads/{{ $channel->slug }}">{{$channel->name}}</a></li>
                                 @endforeach
                             </ul>
