d3d55d60e8cec568729f935c6df74d119b3e5093
Dzalev <dzalev@hotmail.com>
2021-02-15 19:46:57 +0100

    refactor

--- app/Thread.php
+++ app/Thread.php
@@ -8,25 +8,35 @@ class Thread extends Model
 {
 
     protected $guarded = [];
+
     public function path()
     {
         return "/threads/{$this->channel->slug}/{$this->id}";
 //        return '/threads/' . $this->channel->slug . '/' . $this->id;
     }
 
-    public function replies(){
+    public function replies()
+    {
         return $this->hasMany(Reply::class);
     }
 
-    public function creator(){
+    public function creator()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function channel(){
+    public function channel()
+    {
         return $this->belongsTo(Channel::class);
     }
 
-    public function addReply($reply){
+    public function addReply($reply)
+    {
         $this->replies()->create($reply);
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        return $filters->apply($query);
     }
 }
